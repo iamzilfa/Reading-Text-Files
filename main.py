5d9e23cb5ea31e42d2561736b6273eb0808240cd
iamzilfa <zilcyam@gmail.com>
2022-05-27 10:16:48 +0200

    read and count file words

--- main.py
+++ main.py
@@ -3,14 +3,38 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+
+filename = "./story.txt" 
+
 def read_file_content(filename):
     # [assignment] Add your code here 
-    
-    return "Hello World"
 
+    with open(filename, "r") as f:
+        text = f.read()
+        return text
+
+print(read_file_content(filename))
+
+############################################################
 
 def count_words():
-    text = read_file_content("./story.txt")
-    # [assignment] Add your code here
+    str = read_file_content(filename)
 
-    return {"as": 10, "would": 20}
+    # [assignment] Add your code here
+    counts = dict()
+    words = str.split()
+
+    # Loop through each line of the file
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+    return counts
+
+
+# Print the number of occurrences of word
+print(count_words())
+
+
+    
